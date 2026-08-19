01bdd55b9626ef69d283794f1d2a168388d320aa
Tanishka Rathee <tanishkarathee@Tanishkas-MacBook-Air-2.local>
2026-08-19 15:32:59 +0530

    Fix import errors in SettingsScreen and NeckWellUi

--- app/src/main/java/com/humblecoders/neckwell/SettingsScreen.kt
+++ app/src/main/java/com/humblecoders/neckwell/SettingsScreen.kt
@@ -30,6 +30,7 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Switch
 import androidx.compose.material3.SwitchDefaults
 import androidx.compose.material3.Text
+import androidx.compose.material3.Icon
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
